ba843d2e2f3e5788558302f1c68fde97baa24ea1
徐晓伟 <xuxiaowei@live.hk>
2019-05-18 19:31:32 +0800

    新建项目提交。

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Spring-Boot-Session-Redis
-Spring Boot 整合 Session 与 Redis。
+# Spring-Boot-Redis-Session
+Spring Boot 整合 Redis 与 Session 共享。
+
+# 依赖
+
+## Spring Boot 依赖（创建项目时选择）
+
+- Spring Boot
+    - 2.1.5.RELEASE
+    
+- Web
+    - Web                   使用Tomcat和Spring MVC进行全栈Web开发
+    
+- Template
+	- Thymeleaf             Thymeleaf模板引擎（页面）
+    
+- NoSQL
+    - Redis                 Redis键值数据存储，包括spring-data-redis
---
+++ src/main/java/cn/com/xuxiaowei/springbootredissession/SpringBootRedisSessionApplication.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei.springbootredissession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author xuxiaowei
+ */
+@SpringBootApplication
+public class SpringBootRedisSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisSessionApplication.class, args);
+    }
+
+}
